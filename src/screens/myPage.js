0ed13496f66b14a5fbf3fcaa6bca50348028c4d5
Bangdayeon <dybang00@gmail.com>
2024-01-20 00:48:04 -0500

    페이지 이동

--- src/screens/myPage.js
+++ src/screens/myPage.js
@@ -4,24 +4,27 @@ import "react-native-gesture-handler";
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import myPage from "./src/screens/myPage";
-import settings from "./src/screens/settings";
-import report from "./src/screens/report";
-import editInfo from "./src/screens/editInfo";
-
-const stack = createStackNavigator();
-
 export default function App() {
   return (
+      <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>마이 페이지</Text>
 
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="myPage">
-        <Stack.Screen name="myPage" component={myPage}/>
-        <Stack.Screen name="editInfo" component={editInfo}/>
-        <Stack.Screen name="settings" component={settings}/>
-        <Stack.Screen name="report" component={report}/>
-      </Stack.Navigator>
-    </NavigationContainer>
+      <Text
+        style={styles.paragraph}
+        onPress={() => navigation.navigate("editInfo")}>
+        &gt; 회원 정보 수정
+      </Text>
+      <Text
+        style={styles.paragraph}
+        onPress={() => navigation.navigate("settings")}>
+        &gt; 앱 설정 관리
+      </Text>
+      <Text
+        style={styles.paragraph}
+        onPress={() => navigation.navigate("report")}>
+        &gt; 제보하기
+      </Text>
+    </SafeAreaView>
   );
 }
 
