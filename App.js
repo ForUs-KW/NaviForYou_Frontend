8a20661ec63b42efd6291cbac87c72461704a87c
Bangdayeon <dybang00@gmail.com>
2024-01-20 00:28:55 -0500

    코드 수정 및 이동할 페이지 추가

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>마이 페이지</Text>
+
+      <Text
+        style={styles.paragraph}
+        onPress={() => {
+          alert('회원 정보 수정');
+        }}>
+        &gt; 회원 정보 수정
+      </Text>
+      <Text
+        style={styles.paragraph}
+        onPress={() => {
+          alert('앱 설정 관리');
+        }}>
+        &gt; 앱 설정 관리
+      </Text>
+      <Text
+        style={styles.paragraph}
+        onPress={() => {
+          alert('제보하기');
+        }}>
+        &gt; 제보하기
+      </Text>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  title: {
+    margin: 24,
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'left',
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'left',
   },
 });
