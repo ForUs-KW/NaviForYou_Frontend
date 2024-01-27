60ce9d032a1b164ad4541eb46c285b5fb31d69d7
Bangdayeon <dybang00@gmail.com>
2024-01-26 21:45:21 -0500

    add back button

--- App.js
+++ App.js
@@ -1,53 +1,56 @@
 //마이페이지 화면
+import React from 'react';
+import {StatusBar} from 'expo-status-bar';
+import { Image, View, Text, TextInput, SafeAreaView, StyleSheet, Button,} from 'react-native';
 
-import { Text, SafeAreaView, StyleSheet, Button } from 'react-native';
+
+import CustomButton from './src/component/CustomButton.js';
+
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
+    <View>
 
-      <Text style={styles.title}>마이 페이지</Text>
-      <Text
-        style={styles.paragraph}
-        onPress={() => {
-          alert('회원 정보 수정');
-        }}>
-        &gt; 회원 정보 수정
-      </Text>
-      <Text
-        style={styles.paragraph}
-        onPress={() => {
-          alert('앱 설정 관리');
-        }}>
-        &gt; 앱 설정 관리
-      </Text>
-      <Text
-        style={styles.paragraph}
-        onPress={() => {
-          alert('제보하기');
-        }}>
-        &gt; 제보하기
-      </Text>
-    </SafeAreaView>
-  );
+        <CustomButton
+        buttonColor={'skyblue'}
+        titleColor={'black'}
+        title={'<'}
+        onPress={()=> {alert('뒤로가기');}}/>
+
+        <View>
+          <Text style={styles.title}>마이 페이지</Text>
+        </View>
+
+
+      
+      <View>
+        
+        <Text style={styles.content}
+          onPress={() => {alert('회원 정보 수정');}}>&gt; 회원 정보 수정
+        </Text>
+        <Text  style={styles.content}
+         onPress={() => {alert('앱 설정 관리');}}>&gt; 앱 설정 관리
+        </Text>
+        <Text  style={styles.content}
+          onPress={() => {alert('제보하기');}}>&gt; 제보하기
+        </Text>
+      </View>
+
+    </View> //container
+  );//return
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ecf0f1',
-    padding: 8,
-  },
   title: {
-    margin: 24,
+    //marginTop: 30,
+    marginLeft: 20,
     fontSize: 30,
     fontWeight: 'bold',
-    textAlign: 'left',
+
   },
-  paragraph: {
+  content: {
     margin: 24,
     fontSize: 18,
     fontWeight: 'bold',
-    textAlign: 'left',
   },
 });
---
+++ src/component/CustomButton.js
@@ -0,0 +1,50 @@
+//CustomButton.js
+
+import React, { Component } from 'react';
+import { TouchableOpacity, Text, StyleSheet,} from 'react-native';
+
+export default class CustomButton extends Component{
+
+    static defaultProps = {
+        title: 'untitled',
+        buttonColor: 'blue',
+        titleColor: 'black',
+        onPress: () => null,
+      }
+
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+      <TouchableOpacity style={[
+        styles.button,
+        {backgroundColor: this.props.buttonColor}
+      ]}
+      onPress={this.props.onPress}>
+        <Text style={[
+          styles.buttonText,
+          {color: this.props.titleColor}
+        ]}>{this.props.title}</Text>
+      </TouchableOpacity>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor: 'white',
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        width: 60,
+        flexBasis:'auto',
+        //borderRadius: 10,
+        //marginBottom: 10,
+      },
+      buttonText: {
+        color: 'black',
+        fontSize: 30,
+        fontWeight: 'bold',
+      },
+});
