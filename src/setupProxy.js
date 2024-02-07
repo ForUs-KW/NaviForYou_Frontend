aecade11425f9251872b8b300c867c366e8bff4a
Jiwoo Lee <ooketmon@likelion.org>
2024-02-08 02:04:51 +0900

    edit Proxy settings

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,9 @@
 const {createProxyMiddleware} = require('http-proxy-middleware')
 
 module.exports = app => {
-    app.use('/app', createProxyMiddleware({
-        target: 'http://3.34.118.226:8080', // Swagger 서버 주소
+    app.use(createProxyMiddleware('/app', {
+        target: 'http://3.34.118.226:8080', 
         changeOrigin: true,
-        pathRewrite: {
-          '^/app': '', // 프록시된 URL에서 제거할 경로
-        },
-      }));
+        })
+        );
 }
