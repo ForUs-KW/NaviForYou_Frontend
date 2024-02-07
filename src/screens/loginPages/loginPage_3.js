3211d8ae4e6c6e335a7f5351b2ea1f55a77a81da
Jiwoo Lee <ooketmon@likelion.org>
2024-02-08 02:05:32 +0900

    API 연결 - 닉네임 중복 확인

--- src/screens/loginPages/loginPage_3.js
+++ src/screens/loginPages/loginPage_3.js
@@ -30,6 +30,7 @@ const LoginPage_3 = ({navigation})=> {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
+              'Accept': 'application/json'
             },
             body: JSON.stringify({
               nickname,
@@ -37,19 +38,19 @@ const LoginPage_3 = ({navigation})=> {
               email,
               password,
             }),
-            mode: 'no-cors' 
+            //mode: 'no-cors' 
           });
     
           if (response.ok) {
             const data = await response.json();
             // 회원가입 성공 메시지 출력
-            Alert.alert('회원가입 성공', data.message);
+            Alert.alert('회원가입 성공',`'${data.message}'`);
           } else {
             // 회원가입 실패 메시지 출력
             Alert.alert('회원가입 실패', '서버 오류로 회원가입에 실패했습니다.');
           }
         } catch (error) {
-          console.error('회원가입 중 에러:', error);
+          console.error('회원가입 중 에러:', 'error');
           Alert.alert('회원가입 실패', '네트워크 오류로 회원가입에 실패했습니다.');
         }
     };
@@ -60,14 +61,12 @@ const LoginPage_3 = ({navigation})=> {
             const userEmail = email;
     
             // 서버에 이메일 중복 확인을 위한 요청 보내기
-            const response = await fetch('http://3.34.118.226:8080/app/member/emailDuplicate?email={email}', {
-                method: 'POST', // 이메일 중복 확인을 위한 HTTP 메서드 설정
+            const response = await fetch(`http://3.34.118.226:8080/app/member/emailDuplicate?email=${email}`, {
+                method: 'GET', // 이메일 중복 확인을 위한 HTTP 메서드 설정
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({
-                    email: email,
-                }),
+
             });
     
             // 서버 응답 확인
@@ -88,6 +87,38 @@ const LoginPage_3 = ({navigation})=> {
         }
     };
 
+
+    const nicknameDoubleCheck = async () => {
+        try {
+            // 사용자가 입력한 닉네임 가져오기
+            const userNickname = nickname;
+    
+            // 서버에 닉네임 중복 확인을 위한 요청 보내기
+            const response = await fetch(`http://3.34.118.226:8080//app/member/nicknameDuplicate?nickname=${nickname}`, {
+                method: 'GET', // 닉네임 중복 확인을 위한 HTTP 메서드 설정
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+
+            });
+    
+            // 서버 응답 확인
+            if (response.status === 200) {
+                const data = await response.json();
+                if (data.result) {
+                    Alert.alert('사용 가능한 닉네임입니다.');
+                } else {
+                    Alert.alert('이미 사용 중인 닉네임입니다.');
+                }
+            } else {
+                console.error('닉네임 중복 확인 요청 실패:', response.statusText);
+                Alert.alert('닉네임 중복 확인에 실패했습니다.');
+            }
+        } catch (error) {
+            console.error('닉네임 중복 확인 중 에러:', error);
+            Alert.alert('닉네임 중복 확인에 실패했습니다.');
+        }
+    };
 
     return (
         <View >
@@ -119,6 +150,23 @@ const LoginPage_3 = ({navigation})=> {
                         titleSize={14}
                         onPress={()=> emailDoubleCheck() }/>
                 </View>
+
+                <Text style={textStyles.content20}>폰번호</Text>
+                <View style={viewStyles.tabview}>
+                    <TextInput
+                        style={viewStyles.textInputShort}
+                        value={phone}
+                        onChangeText={setPhone}
+                        placeholder="핸드폰 번호를 입력해주세요 (- 제외) "
+                        
+
+                    />
+                </View>
+
+
+
+
+                
 
                 
                 <View>
@@ -166,7 +214,7 @@ const LoginPage_3 = ({navigation})=> {
                         buttonWidth={'25%'}
                         title={'중복확인'}
                         titleSize={14}
-                        onPress={()=> {alert('중복확인');}}/>
+                        onPress={()=> nicknameDoubleCheck() }/>
                 </View>
 
                 <View style={viewStyles.centerItems}>
