c30c5a1de71392ee8c23c21d62780c0220b4b79d
Jiwoo Lee <ooketmon@likelion.org>
2024-02-16 05:24:56 +0900

    ✨ Feat : 로그인 정보 저장
    storeUserData 함수 사용, AsyncStorage에 저장

---
+++ src/auth.js
@@ -0,0 +1,28 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const storeUserData = async (token, userId) => {
+  try {
+    await AsyncStorage.setItem('userData', JSON.stringify({ token, userId }));
+    console.log('User data stored successfully.');
+    //console.log(token);
+    //console.log(userId);
+  } catch (error) {
+    console.error('Error storing user data:', error);
+  }
+};
+
+const getUserData = async () => {
+  try {
+    const userData = await AsyncStorage.getItem('userData');
+    return JSON.parse(userData);
+  } catch (error) {
+    console.error('Error getting user data:', error);
+    return null;
+  }
+};
+
+const test = async(token, userID) =>{
+    console.log('test');
+}
+
+export { storeUserData, getUserData };
